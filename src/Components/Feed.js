1c5d9a198a1e4c1297159e5ce7f33cc507002f06
Michael Tao <zui_tao@berkeley.edu>
2022-10-23 21:23:02 -0700

    finished

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -6,14 +6,20 @@ import Block from './Block';
 const Feed = () => {
     /* TODO: Declare a new state variable to keep track of the blocks on your Blockstagram feed! */
     // Refer to Hint 2 for more help!
+    let [blocks, setBlocks] = useState([]);
 
     /* Use the map() function to render multiple Blocks! */
-    const posts = null; // TODO: edit this variable
+    const posts = blocks.map((c) => {return <Block color={c}></Block>});; // TODO: edit this variable
+
+    const updateBlocks = (color) => {
+        const slice = blocks.slice();
+        slice.unshift(color);
+        setBlocks(slice);
+    }
 
     return (
         <div>
-            <Menu></Menu>
-
+            <Menu func = {updateBlocks}></Menu>
             {/* Below is where all of your Blocks should render! */}
             {posts}
         </div>
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,11 +1,16 @@
 import React from 'react';
 /* Add any imports you think you might need here! */
+import Color from './Color';
 
-const Menu = () => { 
+const Menu = (props) => { 
 
     return (
       <div className="colorOptions">
-          {/* TODO */}
+          <Color color = "red" handleClick={props.func}></Color>
+          <Color color = "pink" handleClick={props.func}></Color>
+          <Color color = "blue" handleClick={props.func}></Color>
+          <Color color = "green" handleClick={props.func}></Color>
+
       </div>
     );
 }
